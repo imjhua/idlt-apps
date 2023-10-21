61c997d2eaacbf6cc47bb519e32431b72d25449a
Lia <imjhua@gmail.com>
2023-10-21 20:43:23 +0900

    add

---
+++ src/feature/기본/pages/index.tsx
@@ -0,0 +1,14 @@
+
+import useTitle from '@/shared/hooks/useTitle'
+
+import Xxx from '../shared/components/Xxx'
+
+function Page(){
+  useTitle()
+  return (
+    <Xxx text="text" />
+  )
+}
+
+export default Page
+
---
+++ src/feature/영어단어테스트/pages/영어단어추가/pages/index.tsx
@@ -0,0 +1,8 @@
+function Page(){
+  return (
+    '영어단어추가'
+  )
+}
+
+export default Page
+
