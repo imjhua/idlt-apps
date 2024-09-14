3e78995da85833e4fd110eeaa421c53fb2bcb06e
imjhua <imjhua@gmail.com>
2024-09-14 14:15:10 +0900

    add

--- src/shared/components/Logo.tsx
+++ src/shared/components/Logo.tsx
@@ -1,19 +1,19 @@
-import styled from '@emotion/styled/macro'
+// import styled from '@emotion/styled/macro'
 
 import Img from './Image'
 
 function Logo(){
   return (
-    <Anchor href="/">
-      <Img src="/logo.png" width={44} height={44} />
-    </Anchor>
+    // <Anchor href="/">
+    <Img src="/logo.png" width={44} height={44} />
+    // </Anchor>
   )
 }
 
 export default Logo
 
-const Anchor = styled.a`
-  border-radius: 4px;
-  text-align: center;
-  padding-left: 6px;
-`
+// const Anchor = styled.a`
+//   border-radius: 4px;
+//   text-align: center;
+//   padding-left: 6px;
+// `
