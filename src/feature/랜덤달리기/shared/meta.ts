580d479b351053f161e738268e22bb51e309f6bc
bella.v <bella.v@kakaomobility.com>
2024-10-01 13:33:29 +0900

    add

--- src/feature/랜덤달리기/shared/meta.ts
+++ src/feature/랜덤달리기/shared/meta.ts
@@ -1,12 +1,12 @@
 export const CHARACTER_LIST = ['거미', '여우', '부엉이', '거북이', '고래', '사슴', '개구리', '사자', '물고기', '양', '돼지', '코끼리']
 
 export enum COUNT_DWON_STATUS {
-  SHOW = 1,
-  HIDDEN = 2,
+  SHOW = 0,
+  HIDDEN = 1,
 }
 
 export enum STATUS {
   READY = 0,
   RUN = 1,
-  END = 1,
+  END = 2,
 }
