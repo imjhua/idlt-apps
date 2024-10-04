d452a852fa170234385734ce79956500cbdec388
bella.v <bella.v@kakaomobility.com>
2024-10-04 14:22:46 +0900

    add

--- src/feature/매일영어문장/shared/meta.ts
+++ src/feature/매일영어문장/shared/meta.ts
@@ -6,6 +6,7 @@ import Day0930 from './assets/0930.json'
 import Day1001 from './assets/1001.json'
 import Day1002 from './assets/1002.json'
 import Day1003 from './assets/1003.json'
+import Day1004 from './assets/1004.json'
 import etc from './assets/etc.json'
 
 // key	SentenceOfTheDay	EnglishSentence	Pattern	PatternMeaning	Example	ExampleMeaning
@@ -30,4 +31,5 @@ export const SENTENCES: { [key in string]: SentencesType } = {
   Day10월01일: Day1001.filter(({ EnglishSentence }) => EnglishSentence),
   Day10월02일: Day1002.filter(({ EnglishSentence }) => EnglishSentence),
   Day10월03일: Day1003.filter(({ EnglishSentence }) => EnglishSentence),
+  Day10월04일: Day1004.filter(({ EnglishSentence }) => EnglishSentence),
 }
