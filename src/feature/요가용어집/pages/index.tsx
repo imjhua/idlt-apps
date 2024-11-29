a64292be5a9be4b3e6b9a63ebe8537419b03ec72
bella.v <bella.v@kakaomobility.com>
2024-11-29 11:18:41 +0900

    add

--- src/feature/요가용어집/pages/index.tsx
+++ src/feature/요가용어집/pages/index.tsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled/macro'
 import { Box, Button, Heading, List, Text, TextInput } from 'grommet'
 import { Sort } from 'grommet-icons'
 import { ChangeEvent, useMemo, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 
 import { GNB_HEIGHT } from '@/shared/components/Gnb'
 import theme from '@/styles/theme'
@@ -29,15 +29,16 @@ const COLORS: Record<string, `#${string}`> = {
 }
 
 function HomePage() {
+  const [searchParams, setSearchParams] = useSearchParams()
   const [isSort, setIsSort] = useState<boolean>(false)
-  const [value, setValue] = useState<string>('')
+  const [searchValue, setSearchValue] = useState<string>(searchParams.get('search') || '')
 
   const filteredList = useMemo(() => {
     const filteredList = 요가용어목록.filter(({ 용어, 용어풀이, 태그 }) => {
       return (
-        용어.toLowerCase().indexOf(value.toLowerCase()) > -1 ||
-        용어풀이.toUpperCase().indexOf(value.toUpperCase()) > -1 ||
-        태그.toLowerCase().indexOf(value.toLowerCase()) > -1
+        용어.toLowerCase().indexOf(searchValue.toLowerCase()) > -1 ||
+        용어풀이.toUpperCase().indexOf(searchValue.toUpperCase()) > -1 ||
+        태그.toLowerCase().indexOf(searchValue.toLowerCase()) > -1
       )
     })
 
@@ -47,7 +48,7 @@ function HomePage() {
       })
     }
     return filteredList
-  }, [value, isSort])
+  }, [searchValue, isSort])
 
   const handleOrderClick = () => {
     setIsSort((state) => !state)
@@ -55,7 +56,7 @@ function HomePage() {
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const search = e.target.value
-    setValue(search)
+    setSearchValue(search)
   }
 
   return (
@@ -72,13 +73,16 @@ function HomePage() {
           <Box direction="row" justify="between">
             <Heading level={2}>요가 용어집</Heading>
             <Button primary={isSort} style={{ padding: 4, borderRadius: 4 }}>
-              <SortIcon sort={isSort} onClick={handleOrderClick} />
+              <SortIcon sorted={isSort} onClick={handleOrderClick}>
+                <Sort />
+              </SortIcon>
             </Button>
           </Box>
           <TextInput
             name="name"
             placeholder={<Text>검색</Text>}
             onChange={handleInputChange}
+            value={searchValue}
         />
         </Box>
         <List
@@ -89,6 +93,9 @@ function HomePage() {
                 <Link
                   key={index}
                   to={{ pathname: 'detail', search: `?index=${index + 1}` }}
+                  onClick={() => {
+                    setSearchParams({ search: searchValue })
+                  }}
                 >
                   <Text size="large">{용어}</Text>
                   {태그 && (
@@ -130,8 +137,8 @@ function HomePage() {
 
 export default HomePage
 
-const SortIcon = styled(Sort)<{ sort: boolean }>`
-  ${({ theme, sort }) => sort &&
+const SortIcon = styled.span<{ sorted: boolean }>`
+  ${({ theme, sorted }) => sorted &&
     css`
       stroke: ${theme.background};
     `};
